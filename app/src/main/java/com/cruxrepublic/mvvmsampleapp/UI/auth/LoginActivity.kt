2c5051d134f7ab1ec976858ce373a02f55632281
dancrux <nwogwugwudaniel@gmail.com>
2020-02-23 22:29:19 +0100

    Initial commit

---
+++ app/src/main/java/com/cruxrepublic/mvvmsampleapp/UI/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.cruxrepublic.mvvmsampleapp.UI.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cruxrepublic.mvvmsampleapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/cruxrepublic/mvvmsampleapp/UI/auth/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.cruxrepublic.mvvmsampleapp.UI.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cruxrepublic.mvvmsampleapp.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
